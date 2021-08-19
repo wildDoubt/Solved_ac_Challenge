f2d45974e07095e7c98ca137ecd7e606d2a31d38
Wonyeong Choi <coma01155@gmail.com>
2021-08-19 21:06:26 +0900

    update

--- Algorithm/boj22899.cpp
+++ Algorithm/boj22899.cpp
@@ -1,12 +1,6 @@
-//#define LOCAL
-
 #include <iostream>
 #include <algorithm>
-#include <queue>
-#include <set>
-#include <map>
 #include <unordered_map>
-//#include <ctime>
 
 using namespace std;
 using p = pair<int, int>;
@@ -20,75 +14,62 @@ inline void Quick_IO() {
 }
 
 int A[MAX];
-p arr[MAX]; // <b, number>
+p arr[MAX];
 
 int main() {
-//#ifdef LOCAL
-//    freopen("boj22899_input.txt", "r", stdin);
-//    freopen("boj22899_output.txt", "w", stdout);
-//#endif
     Quick_IO();
-//    auto start = clock();
     int N, K;
     cin >> N >> K;
 
-    for (int i = 0; i < N; ++i) {
-        cin >> A[i];
-    }
+    for (int i = 0; i < N; ++i) cin >> A[i];
 
-    for (int i = 0; i < N; ++i) {
-        int a;
+    for (int i = 0, a; i < N; ++i) {
         cin >> a;
         arr[i] = p(a, A[i]);
     }
 
-    sort(arr, arr + N, [](p a, p b) {
-        if (a.first == b.first) {
-            return a.second > b.second;
-        }
-        return a.first < b.first;
-    });
+    sort(arr, arr + N, [](p a, p b) { return a.first < b.first; });
 
-    unordered_map<int, int> M1;
-    unordered_map<int, int> M2;
-    for (int i = 0; i < N; ++i) M1[arr[i].second]++;
-    for (int i = 0; i < K; ++i) M2[arr[i].second]++;
-    int limit = (*max_element(M2.begin(), M2.end(), [](p a, p b) {
-        return a.second < b.second;
-    })).second;
+    unordered_map<int, int> M;
+    int limit;
+    ll answer;
     ll limitAnswer = 0;
-    for (int i = 0; i < K; ++i) {
-        limitAnswer += arr[i].first;
+
+    for (int i = 0; i < N; ++i) {
+        M[arr[i].second]++;
+        if (i == K-1) {
+            limit = (*max_element(M.begin(), M.end(), [](p a, p b) {
+                return a.second < b.second;
+            })).second;
+        }
+        if (i < K) {
+            limitAnswer += arr[i].first;
+        }
     }
-    for (int i = 1; i <= N; ++i) {
+
+    for (int i = 1; i <= N; ++i, answer = 0) {
         if (i >= limit) {
-            cout << limitAnswer;
+            answer = limitAnswer;
         } else {
             int temp1 = 0;
-            for (auto x:M1) {
+            for (auto x:M) {
                 temp1 += min(i, x.second);
             }
             if (temp1 < K) {
-                cout << -1 << " ";
-                continue;
-            }
-            unordered_map<int, int> temp;
-            int counts = 0;
-            ll answer = 0;
-            int index = 0;
-            while (counts < K) {
-                if (temp[arr[index].second] < i) {
-                    answer += arr[index].first;
-                    temp[arr[index].second]++;
-                    counts++;
+                answer = -1;
+            } else {
+                unordered_map<int, int> temp;
+                int counts = 0;
+                for (int index = 0; counts < K; ++index) {
+                    if (temp[arr[index].second] < i) {
+                        answer += arr[index].first;
+                        temp[arr[index].second]++;
+                        counts++;
+                    }
                 }
-                index++;
             }
-            cout << answer;
         }
-        cout << " ";
+        cout << answer << ' ';
     }
-//    auto end = clock();
-//    cout<<'\n'<<(double)(end-start)/CLOCKS_PER_SEC<<"s\n";
     return 0;
 }
